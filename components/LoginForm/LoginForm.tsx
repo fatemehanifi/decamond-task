eed3473607804aa34f4b867f3f3e028bcd07d2ce
Fateme Hanifi <85929843+fatemehanifi@users.noreply.github.com>
2025-07-19 05:27:09 +0330

    feat: logging users in

--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -1,15 +1,66 @@
 "use client"
 
+import React from "react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { getRequest } from "@/lib/apiCalls";
+
 import Input from "@/components/Input/Input";
 import Button from "@/components/Button/Button";
 
 import styles from "./LoginForm.module.scss";
 
+type User = {
+    results: [],
+    info: {
+        page: number,
+        results: number,
+        seed: string,
+        version: string
+    }
+}
+
 export default function LoginForm() {
+    const router = useRouter();
+    const [phone, setPhone] = useState<string>("");
+    const [btnLoading, setBtnLoading] = useState<boolean>(false);
+
+    const handleSubmit = async () => {
+        const phoneRegex = /^(09\d{9}|9\d{9})$/;
+        if (phoneRegex.test(phone)) {
+            try {
+                setBtnLoading(true);
+                const user: User | null =
+                    await getRequest<User>('https://randomuser.me/api/?results=1&nat=us');
+
+                if (user) localStorage.setItem("user_info", user.info.seed)
+
+                setBtnLoading(false);
+                router.push("/dashboard");
+
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        } else {
+            alert("Please enter a valid phone number!!!");
+        }
+    }
+
     return (
         <div className={styles.formWrapper}>
-            <Input label={"phone number"} name={"phone"} value={"09386005853"} onChange={() => {}} />
-            <Button loading={false} text={"Submit"} disabled={false} onClick={() =>{}} />
+            <Input
+                label={"phone number"}
+                name={"phone"}
+                value={phone}
+                onChange={e => setPhone(e.target.value)}
+            />
+            <Button
+                text={"Submit"}
+                disabled={false}
+                loading={btnLoading}
+                onClick={handleSubmit}
+            />
         </div>
     )
 }
